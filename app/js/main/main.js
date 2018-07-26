2318511302f95eddaf950fd8da1fe4ac6dd6b40a
RemyaAlroy <remyaalroy@gmail.com>
2018-07-26 19:21:33 +0800

    Update main.js

--- app/js/main/main.js
+++ app/js/main/main.js
@@ -3,13 +3,21 @@ $( document ).ready( function(){
 });
 $('#dropdown-submenu').click( function(){
   var getClass = document.getElementsByClassName("limobilesub");
+  var getId = document.getElementById('dropdown-submenu');
+  var arrowCheck = document.getElementsByClassName('uparrow');
   console.log(getClass);
   for(var i =0;i< getClass.length; i++){
     if ( getClass[i].style.display == 'none'){
       getClass[i].style.display = "block";
+      getId.style.color = '#963737';
+      arrowCheck[2].style.transform = 'rotate(-135deg)';
+     
     }
     else{
       getClass[i].style.display = 'none';
+      getId.style.color = 'black';
+      arrowCheck[2].style.transform = 'rotate(45deg)';
+      
     }
   }
 })
